67a8041443d3aa86f2e925836723d759896b35db
Caleb Dewey <calebdewey@Calebs-MacBook-Pro.local>
2023-03-29 17:26:30 -0600

    created basic StudentDashboard layout

---
+++ src/components/StudentDashboard/StudentDashboard.tsx
@@ -0,0 +1,67 @@
+import "./StudentDashboard.css";
+import { useState } from 'react';
+
+export function StudentChecklist () {
+
+    const checkList = ["Get separation orders", "Turn in gear", "Separation physical", "Separation brief", "Pick up DD214", "Clear installation", "Clear unit"];
+    const [checked, setChecked] = useState([]); 
+    const handleCheck = (e) => {
+        let updatedList = [...checked];
+        if (e.target.checked) {
+            updatedList = [...checked, e.target.value]
+        } else {
+            updatedList.splice(checked.indexOf(e.target.value), 1);
+        }
+        setChecked(updatedList);
+    }
+
+    return (
+        <div className="main-container">
+            <div className="outprocessing-container">
+                <h2 className="column-title">
+                    Outprocessing Progress
+                </h2>
+                <div className="student-section">
+                    <h3 
+                        id="student-name"
+                        className="student-info"
+                    >
+                        First Last
+                    </h3>
+                    <h3 
+                        id="branch"
+                        className="student-info"
+                    >
+                        Branch
+                    </h3>
+                    <h3 
+                        id="sep-date"
+                        className="student-info"
+                    >
+                        Separation Date: Date
+                    </h3>
+                </div>
+                <div className="checklist-container">
+                    <ul className="task">
+                       {checkList.map((item, index) => (
+                        <div className="task-item" key={index}>
+                            <input value={item} type="checkbox" onChange={handleCheck} />
+                            <span>{item}</span>
+                        </div>
+                       ))}
+                    </ul>
+                </div>
+            </div>
+            <div className="notes-container">
+                <h2 className="column-title">
+                    Notes
+                </h2>
+                <div className="notes-section">
+                    Input notes here
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default StudentChecklist
