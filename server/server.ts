c9db743618e88419e89f6b8b33fcd9eba34dac6a
Tom <tmschutter@gmail.com>
2023-03-30 10:30:55 -0600

    server barebones setup

--- server/server.ts
+++ server/server.ts
@@ -1,3 +1,25 @@
-declare function require(name:string);
 const express = require('express')
+const morgan = require('morgan')
+const postgres = require('postgres')
+const dotenv = require('dotenv')
+const cors = require('cors')
+
+dotenv.config()
+const PORT = process.env.PORT
+
 const app = express()
+const sql = postgres(process.env.DATABASE_URL)
+
+app.use(express.json())
+app.use(morgan('tiny'))
+app.use(cors('*'))
+
+app.use(express.static('../dist'))
+
+app.get('/test', (req: any, res: any): void => {
+   res.send('working')
+})
+
+app.listen(PORT, () => {
+   console.log(`listening on port ${PORT}`)
+})
