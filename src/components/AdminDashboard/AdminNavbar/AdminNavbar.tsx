3daa9147d795c2c01fd7a02bff768ea8365b22bb
mattcmoore <matt@mattcmoore.com>
2023-03-30 09:52:24 -0700

    svg

--- src/components/AdminDashboard/AdminNavbar/AdminNavbar.tsx
+++ src/components/AdminDashboard/AdminNavbar/AdminNavbar.tsx
@@ -20,7 +20,19 @@ const AdminNavbar = () => {
             <div className={`admin-dropdown ${mouseover ? 'mouseover' : ''}`}>
                 <div className="admin-dropdown-btn" onMouseEnter={()=>setMouseover(true)}>
                     <p className="admin-dropdown-avatar">AA</p>
-                    <p>ADMIN-NAME</p><p>triangle</p>
+                    <p>ADMIN-NAME</p>
+                        <svg id="triangle" viewBox="0 0 232.72 86.82">
+                            <defs>
+                                <style>
+                                    .cls-1 {
+                                        fill: #231f20;
+                                        stroke: #fff;
+                                        stroke-miterlimit: 10;
+                                    }
+                                </style>
+                            </defs>
+                        <path class="cls-1" d="M116.36,86.19L1.51,.5H231.21L116.36,86.19Z"/>
+                        </svg>
                 </div>
                 <div className={mouseover ? 'admin-dropdown-account' : 'hidden' }>
                     <p>MY ACCOUNT</p>
