5dc8f274609b2ce88ad720619dde58e99a508e9a
mattcmoore <matt@mattcmoore.com>
2023-04-12 16:54:07 -0700

    username shows up in nav

--- src/components/AdminDashboard/AdminNavbar/AdminNavbar.tsx
+++ src/components/AdminDashboard/AdminNavbar/AdminNavbar.tsx
@@ -4,7 +4,7 @@ import BtbContext from '../../../context/BtbContext'
 import { useState, useContext } from "react";
 
 const AdminNavbar = () => {
-    const {setAdminModal} = useContext(BtbContext)
+    const {setAdminModal, user, logOut} = useContext(BtbContext)
     const [mouseover, setMouseover] = useState(false)
 
   const handleMouseover = () => {
@@ -21,8 +21,8 @@ const AdminNavbar = () => {
       </div>
       <div className={`admin-dropdown ${mouseover ? 'mouseover' : ''}`}>
           <div className="admin-dropdown-btn" onMouseEnter={()=>setMouseover(true)}>
-              <p className="admin-dropdown-avatar">AA</p>
-              <p>ADMIN-NAME</p>
+              <p className="admin-dropdown-avatar">{user.name.match(/\b\w/g).join("")}</p>
+              <p>{user.name}</p>
                   <svg className="triangle" viewBox="0 0 232.72 115">
                       <path className="cls-1" d="M116.02,120.76L1.17,.5H230.88L116.02,120.76Z"/>
                   </svg>
