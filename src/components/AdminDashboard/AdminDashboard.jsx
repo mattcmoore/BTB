1761c3ca37f3494c1c234a9912aeb4a9dfda3495
mattcmoore <matt@mattcmoore.com>
2023-04-13 11:27:07 -0700

    cleanup

--- src/components/AdminDashboard/AdminDashboard.jsx
+++ src/components/AdminDashboard/AdminDashboard.jsx
@@ -1,12 +1,13 @@
 import './AdminDashboard.css'
-import AdminNavbar from './AdminNavbar/AdminNavbar.jsx'
+import AdminNavbar from './AdminNavbar/AdminNavbar'
+import Admins from './Admins/Admins'
 
 export const AdminDashboard = () => {
     return(
         <div className="admin-dashboard-container">
             <AdminNavbar />
-            <div className="admin-dashboard-container-body">
-                
+            <div className="admin-dashboard-container-body"> 
+                <Admins />
             </div>
         </div>
     )
